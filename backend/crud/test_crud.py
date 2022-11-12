2065f2738871834e238f574626557f66db75ee99
Bharati Kulkarni <bmk15897@gmail.com>
2022-11-11 20:50:48 -0600

    Added Basic Apis

--- backend/crud/test_crud.py
+++ backend/crud/test_crud.py
@@ -1,10 +1,54 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from sqlalchemy import text
+from json import loads
+from collections import defaultdict
+import itertools
+import datetime
 
 def get_users_test(db: Session):
     query = text('select * from Users')
     result = db.execute(query)
-    names = [row[0] for row in result]
+    names = result.fetchall()
     print(names)
-    return {"Hehe": True}
+    return {"users": names}
+
+def verify_login(db: Session, emailAddress, pwdHash):
+    query = text('select * from Users where emailAddress=\''+emailAddress+'\' and pwdHash=\''+pwdHash+'\'')
+    result = db.execute(query)
+    user = result.fetchall()
+    if len(user)==0:
+        return {"status": False, "message": "Incorrect Email/Pwd"}
+    return {"status": True, "message": "Successful", "user": user[0]}
+
+def get_all_processes_for_a_department(db: Session, emailAddress):
+    # query = text('select processId, processName, wfInstanceId from Processes p join (select max(seqNumber) as maxSeq, w.wfId as wfId, wfInstanceId from WorkflowInstances w join ProcessInstances using (wfInstanceId) join Processes using (processId) group by wfInstanceId) as aP on (aP.maxSeq = p.seqNumber and aP.wfId = p.wfId) where deptId = 1;')
+    temp = db.execute('select userId,businessId from Users where emailAddress=\''+emailAddress+'\'')
+    userId = temp.first()['userId']
+    businessId = temp.first()['businessId']
+
+    deptId = '(select deptId from UserDepartment where userId = (select userId from Users where emailAddress=\''+emailAddress+'\'))'
+    pri = '(select * from ProcessInstances where CAST(ProcessInstances.completedDT AS CHAR(20)) =\'0000-00-00 00:00:00\')'
+    query = text('select * from (select * from Workflows where businessId = ('+businessId+')) as wf join WorkflowInstances on wf.wfId join (select * from Processes where Processes.deptId='+deptId+') as pro on pro.wfId join '+pri+' as pri on pri.processId join Parameters on Parameters.paramId join ParamInstances on ParamInstances.paramId')
+    print(query)
+    result = db.execute(query)
+    processes = list(result.fetchall())
+    # for key, group in itertools.groupby(processes, lambda item: item["wfInstanceId"]):
+    #     print(key, [item for item in group])
+    return processes
+
+def add_workflow_instance(db: Session, wfId):
+    createdDT = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    wfInstanceId = int(db.execute(text('select max(wfInstanceId) from WorkflowInstances')).first()[0])+1
+    # print(wfInstanceId[0])
+    query = text('insert into WorkflowInstances (wfInstanceId,createdDT,wfId) values ('+str(wfInstanceId)+',\''+createdDT+'\','+str(wfId)+')');
+    result = db.execute(query)
+    return "List of workflows with details"
+
+def delete_workflow_instance(db: Session, wfInstanceId):
+    #Delete query
+    query = text('delete from WorkflowInstances where wfInstanceId='+wfInstanceId)
+    return "deleted"
+
+def complete_process_instance(db: Session, processInstanceId, params):
+    return "Completed"
--- backend/routers/test_router.py
+++ backend/routers/test_router.py
@@ -13,7 +13,22 @@ router = APIRouter(
 async def get_users_test(db: Session = Depends(get_db)):
     return test_crud.get_users_test(db=db)
 
-@router.get("/workflows")
-async def get_workflows_test(db: Session = Depends(get_db)):
-    return test_crud.get_users_test(db=db)
+@router.get("/userLogin")
+async def get_user_login(db: Session = Depends(get_db)):
+    return test_crud.verify_login(db=db,emailAddress='samarthgupta@gmail.com', pwdHash="abcd123")
 
+@router.get("/userDeptProcesses")
+async def get_all_processes_for_a_department(db: Session = Depends(get_db)):
+    return test_crud.get_all_processes_for_a_department(db=db,emailAddress='bkulka@gmail.com')
+
+@router.get("/workflow/addInstance")
+async def add_workflow_instance(db: Session = Depends(get_db)):
+    return test_crud.add_workflow_instance(db=db,wfId=100)
+
+@router.get("/workflow/deleteInstance")
+async def delete_workflow_instance(db: Session = Depends(get_db)):
+    return test_crud.delete_workflow_instance(db=db,wfInstanceId=100)
+
+@router.get("/process/completeInstance")
+async def complete_process_instance(db: Session = Depends(get_db)):
+    return test_crud.complete_process_instance(db=db,processInstanceId=100, params={})
