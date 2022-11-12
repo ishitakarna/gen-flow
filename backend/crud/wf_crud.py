1a92e041ffbaeaad92e772fee5a55083fec52aa9
Bharati Kulkarni <bmk15897@gmail.com>
2022-11-12 13:27:51 -0600

    Search Functionality and removed limit

--- backend/crud/wf_crud.py
+++ backend/crud/wf_crud.py
@@ -10,7 +10,7 @@ def get_workflow_types_for_business(db: Session, businessId: int):
     return workflow_obj_arr
 
 def get_incomplete_workflow_instances_for_business(db: Session, businessId: int):
-    wf_ins_join_wf = 'select wfInstanceId, createdDT as wfcreatedDT, updatedDT as wfupdatedDT, completedDT as wfcompletedDT, Workflows.wfId, wfName, wfDescription, businessId from WorkflowInstances join Workflows on Workflows.wfId = WorkflowInstances.wfId where completedDT = \"2001-01-01T00:00:00\" and businessId = {businessId} order by createdDT desc limit 10'.format(businessId = businessId)
+    wf_ins_join_wf = 'select wfInstanceId, createdDT as wfcreatedDT, updatedDT as wfupdatedDT, completedDT as wfcompletedDT, Workflows.wfId, wfName, wfDescription, businessId from WorkflowInstances join Workflows on Workflows.wfId = WorkflowInstances.wfId where completedDT = \"2001-01-01T00:00:00\" and businessId = {businessId} order by createdDT desc'.format(businessId = businessId)
     query = text('select * from Processes join ProcessInstances on Processes.processId = ProcessInstances.processId join ({wf_ins_join_wf}) as ABC on ABC.wfId = Processes.wfId and ABC.wfInstanceId = ProcessInstances.wfInstanceId left join Parameters on Parameters.processId = Processes.processId left join (select paramVal, processInstanceId as prProcessInstanceId, paramId as prParamId from ParamInstances) as ParamInstancesSQ on Parameters.paramId = ParamInstancesSQ.prParamId and ParamInstancesSQ.prProcessInstanceId = ProcessInstances.processInstanceId'.format(wf_ins_join_wf=wf_ins_join_wf))
     print(query)
     result = db.execute(query)
@@ -37,3 +37,22 @@ def add_workflow_instance(db: Session, wfId: int, businessId: int):
     query3 = text('insert into ProcessInstances(createdDT, completedDT, processId, wfInstanceId) values (\''+createdDT+'\',\''+str(datetime.datetime.strptime('2001-01-01 00:00:00', "%Y-%m-%d %H:%M:%S"))+'\','+str(processId)+','+str(wfInstanceId)+')')
     db.execute(query3)
     return get_incomplete_workflow_instances_for_business(db,businessId=businessId)
+
+def search_workflowInstance_by_wfInstanceId(db: Session, wfInstanceId: int):
+    wf_ins_join_wf = 'select wfInstanceId, createdDT as wfcreatedDT, updatedDT as wfupdatedDT, completedDT as wfcompletedDT, Workflows.wfId, wfName, wfDescription, businessId from WorkflowInstances join Workflows on Workflows.wfId = WorkflowInstances.wfId where wfInstanceId={wfInstanceId} order by createdDT desc'.format(wfInstanceId=wfInstanceId)
+    query = text(
+        'select * from Processes join ProcessInstances on Processes.processId = ProcessInstances.processId join ({wf_ins_join_wf}) as ABC on ABC.wfId = Processes.wfId and ABC.wfInstanceId = ProcessInstances.wfInstanceId left join Parameters on Parameters.processId = Processes.processId left join (select paramVal, processInstanceId as prProcessInstanceId, paramId as prParamId from ParamInstances) as ParamInstancesSQ on Parameters.paramId = ParamInstancesSQ.prParamId and ParamInstancesSQ.prProcessInstanceId = ProcessInstances.processInstanceId'.format(
+            wf_ins_join_wf=wf_ins_join_wf))
+    print(query)
+    result = db.execute(query)
+    workflow_obj_arr = list(result.fetchall())
+    hashmap = {}
+    for key, group in itertools.groupby(workflow_obj_arr, lambda item: item["wfInstanceId"]):
+        hashmap[key] = []
+        # hashmap[key] = [item for item in group]
+        hashmap1 = {}
+        for key1, group1 in itertools.groupby(group, lambda item1: item1["processInstanceId"]):
+            # print(key1, group1)
+            hashmap1[key1] = [item1 for item1 in group1]
+        hashmap[key].append(hashmap1)
+    return hashmap
--- backend/routers/wf_router.py
+++ backend/routers/wf_router.py
@@ -14,9 +14,13 @@ async def get_workflow_types_for_business(businessId: int, db: Session = Depends
     return wf_crud.get_workflow_types_for_business(db, businessId)
 
 @router.get("/instances/incomplete/{businessId}")
-async def abc(businessId: int, db: Session = Depends(get_db)):
+async def get_incomplete_workflow_instances_for_business(businessId: int, db: Session = Depends(get_db)):
     return wf_crud.get_incomplete_workflow_instances_for_business(db, businessId=businessId)
 
 @router.post("/instances/add/{wfId}/{businessId}")
-async def abc(wfId: int, businessId: int, db: Session = Depends(get_db)):
+async def add_workflow_instance(wfId: int, businessId: int, db: Session = Depends(get_db)):
     return wf_crud.add_workflow_instance(db, wfId=wfId, businessId=businessId)
+
+@router.get("/instances/search/{wfInstanceId}")
+async def search_workflowInstance_by_wfInstanceId(wfInstanceId: int, db: Session = Depends(get_db)):
+    return wf_crud.search_workflowInstance_by_wfInstanceId(db, wfInstanceId = wfInstanceId)
