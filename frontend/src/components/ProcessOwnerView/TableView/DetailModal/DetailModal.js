87ddb41e7464d6125767a1584e497d1453acc93e
Ishita Karna <ishitakarna@gmail.com>
2022-11-12 15:54:07 -0600

    detail view added

--- frontend/src/components/ProcessOwnerView/TableView/DetailModal/DetailModal.js
+++ frontend/src/components/ProcessOwnerView/TableView/DetailModal/DetailModal.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 function DetailModal({wfInst, onHide}) {
     const api = new Api();
-    const [workflowDetails, setWorkflowDetails] = useState({});
+    const [workflowDetails, setWorkflowDetails] = useState("");
 
     useEffect(() => {
         getWorkflowDetails();
@@ -24,23 +24,26 @@ function DetailModal({wfInst, onHide}) {
                     let len = processes[key].length
                     let temp = processes[key][len - 1]
                     console.log(temp)
-                    let val = Object.values(temp)[0][0]
-                    //console.log(val)
-                    wf.wfInstanceId = val.wfInstanceId
-                    wf.name = val.wfName
-                    wf.dateC = val.wfcreatedDT
-                    wf.dateU = val.wfupdatedDT
-                    wf.curP = val.processId
-                    wf.dept = val.deptId
-                    wf.businessId = val.businessId
-                    wfData.push(wf)
+                    wfData.push(JSON.stringify(temp))
+                    // let val = Object.values(temp)[0][0]
+                    // wfData.push(JSON.stringify(val))
+                    // //console.log(val)
+                    // wf.wfInstanceId = val.wfInstanceId
+                    // wf.name = val.wfName
+                    // wf.dateC = val.wfcreatedDT
+                    // wf.dateU = val.wfupdatedDT
+                    // wf.curP = val.processId
+                    // wf.dept = val.deptId
+                    // wf.businessId = val.businessId
+                    // wfData.push(wf)
                 })
-                //setWorkflowDetails(test);
+                setWorkflowDetails(wfData);
         })
     }
 
     return (
         <>
+        {workflowDetails}
         <Modal.Footer>
           <Button onClick={onHide}>{"Close"}</Button>
         </Modal.Footer>
