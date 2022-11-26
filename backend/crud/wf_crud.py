40b6ba1fa625b51f277a6ff60006935ec4c90731
Bharati Kulkarni <bmk15897@gmail.com>
2022-11-26 17:28:52 -0600

    Sqaushy Squishy

--- backend/crud/wf_crud.py
+++ backend/crud/wf_crud.py
@@ -10,22 +10,57 @@ def get_workflow_types_for_business(db: Session, businessId: int):
     return workflow_obj_arr
 
 def get_incomplete_workflow_instances_for_business(db: Session, businessId: int):
-    wf_ins_join_wf = 'select wfInstanceId, createdDT as wfcreatedDT, updatedDT as wfupdatedDT, completedDT as wfcompletedDT, Workflows.wfId, wfName, wfDescription, businessId from WorkflowInstances join Workflows on Workflows.wfId = WorkflowInstances.wfId where completedDT = \"2001-01-01T00:00:00\" and businessId = {businessId}'.format(businessId = businessId)
-    query = text(
-        'select * from Processes join ProcessInstances on Processes.processId = ProcessInstances.processId join ({wf_ins_join_wf}) as ABC on ABC.wfId = Processes.wfId and ABC.wfInstanceId = ProcessInstances.wfInstanceId left join (select processId as paramProcessId, paramName, paramId from Parameters) as params on params.paramProcessId = Processes.processId left join (select paramVal, processInstanceId as prProcessInstanceId, paramId as prParamId from ParamInstances) as ParamInstancesSQ on params.paramId = ParamInstancesSQ.prParamId and ParamInstancesSQ.prProcessInstanceId = ProcessInstances.processInstanceId order by ABC.wfupdatedDT desc'.format(
-            wf_ins_join_wf=wf_ins_join_wf))
-    print(query)
-    result = db.execute(query)
+    view_j1_wf_ins_join_wf = 'create or replace view j1WfInsJoinWf as select wfInstanceId as j1WfInstanceId, createdDT as wfcreatedDT, updatedDT as wfupdatedDT, completedDT as wfcompletedDT, Workflows.wfId as j1WfId, wfName, wfDescription, businessId as j1BusinessId from WorkflowInstances join Workflows on Workflows.wfId = WorkflowInstances.wfId where completedDT = \"2001-01-01T00:00:00\" and businessId = {businessId}'.format(
+        businessId=businessId)
+    view_j2_pro_ins_join_pro = 'create or replace view j2ProInsJoinPro as select Processes.processId as j2ProcessId, processName, seqNumber, wfId as j2WfId, processInstanceId as j2ProcessInstanceId, createdDT as processInstanceCreatedDT, completedDT as processInstanceCompletedDT, wfInstanceId as j2WfInstanceId from Processes join ProcessInstances on Processes.processId = ProcessInstances.processId'
+    view_j3_j1_join_j2 = 'create or replace view j3J1JoinJ2 as select * from j1WfInsJoinWf join j2ProInsJoinPro on j1WfInsJoinWf.j1WfId = j2ProInsJoinPro.j2WfId and j1WfInsJoinWf.j1WfInstanceId = j2ProInsJoinPro.j2WfInstanceId'
+    view_j4_j3_join_params = 'create or replace view j4J3JoinParam as select * from j3J1JoinJ2 join Parameters on j3J1JoinJ2.j2ProcessId = Parameters.processId'
+    view_j5_j4_join_param_ins = 'create or replace view j5J4JoinParamIns as select j1WfInstanceId as wfInstanceId, wfcreatedDT, wfupdatedDT, wfcompletedDT, j1WfId as wfId, wfName, wfDescription, j1BusinessId as businessId, j2ProcessId as processId, processName, seqNumber, j2ProcessInstanceId as processInstanceId, processInstanceCreatedDT, processInstanceCompletedDT, j4J3JoinParam.paramId as paramId, paramName, paramType, isOptional, paramVal from j4J3JoinParam left join ParamInstances on j4J3JoinParam.paramId = ParamInstances.paramId and j4J3JoinParam.j2ProcessInstanceId = ParamInstances.processInstanceId'
+
+    result = db.execute(view_j1_wf_ins_join_wf)
+    result = db.execute(view_j2_pro_ins_join_pro)
+    result = db.execute(view_j3_j1_join_j2)
+    result = db.execute(view_j4_j3_join_params)
+    result = db.execute(view_j5_j4_join_param_ins)
+    result = db.execute('select * from j5J4JoinParamIns')
+
     workflow_obj_arr = list(result.fetchall())
-    hashmap = {}
+    hashmap = []
     for key, group in itertools.groupby(workflow_obj_arr, lambda item: item["wfInstanceId"]):
-        hashmap[key] = []
-        # hashmap[key] = [item for item in group]
-        hashmap1 = {}
+        group,tp = itertools.tee(group)
+        tp = list(tp)
+        temp = {}
+        temp["wfInstancceId"] = key
+        temp["wfId"] = tp[0]["wfId"]
+        temp["businessId"] = tp[0]["businessId"]
+        temp["wfcreatedDT"] = tp[0]["wfcreatedDT"]
+        temp["wfupdatedDT"] = tp[0]["wfupdatedDT"]
+        temp["wfcompletedDT"] = tp[0]["wfcompletedDT"]
+        temp["wfName"] = tp[0]["wfName"]
+        temp["wfDescription"] = tp[0]["wfDescription"]
+        hashmap1 = []
         for key1,group1 in itertools.groupby(group, lambda item1: item1["processInstanceId"]):
-            # print(key1, group1)
-            hashmap1[key1] = [item1 for item1 in group1]
-        hashmap[key].append(hashmap1)
+            group1, tp1 = itertools.tee(group1)
+            tp1 = list(tp1)
+            temp2 = {}
+            temp2["processInstanceId"] = key1
+            temp2["paramInstances"] = []
+            temp2["processInstanceCreatedDT"] = tp1[0]["processInstanceCreatedDT"]
+            temp2["processInstanceCompletedDT"] = tp1[0]["processInstanceCompletedDT"]
+            temp2["seqNumber"] = tp1[0]["seqNumber"]
+            temp2["processName"] = tp1[0]["processName"]
+            temp2["processId"] = tp1[0]["processId"]
+            for item1 in group1:
+                tp3 = {}
+                tp3["paramId"] = item1["paramId"]
+                tp3["paramName"] = item1["paramName"]
+                tp3["paramType"] = item1["paramType"]
+                tp3["isOptional"] = item1["isOptional"]
+                tp3["paramVal"] = item1["paramVal"]
+                temp2["paramInstances"].append(tp3)
+            hashmap1.append(temp2)
+        temp["processInstances"] = hashmap1
+        hashmap.append(temp)
     return hashmap
 
 def add_workflow_instance(db: Session, wfId: int, businessId: int):
