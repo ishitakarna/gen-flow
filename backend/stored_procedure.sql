c50c81b76128840eb0da8a1c21379384fbaa4957
Samarth Gupta <samarthgupta@Samarths-MacBook-Air.local>
2022-11-26 21:23:58 -0600

    Modified SP file

--- backend/stored_procedure.sql
+++ backend/stored_procedure.sql
@@ -1,11 +1,11 @@
 delimiter //
-create procedure AddWorkflowInstance (IN wfId INT)
+create procedure AddWorkflowInstance (IN workflowId INT)
 begin
     declare varWfInstanceId INT;
     declare varProcessId INT;
-    insert into WorkflowInstances(createdDT, wfId, updatedDT, completedDT) values (NOW(), wfId, '2001-01-01T00:00:00', '2001-01-01T00:00:00'); 
+    insert into WorkflowInstances(createdDT, wfId, updatedDT, completedDT) values (NOW(), workflowId, '2001-01-01T00:00:00', '2001-01-01T00:00:00'); 
     select max(wfInstanceId) into @varWfInstanceId from WorkflowInstances;
-    select processId into @varProcessId from Processes where seqNumber = 1 and wfId = @wfId;
+    select processId into @varProcessId from Processes where seqNumber=1 and wfId=workflowId;
     insert into ProcessInstances(createdDT, completedDT, processId, wfInstanceId) values
         (NOW(), '2001-01-01T00:00:00', @varProcessId, @varWfInstanceId);
 end //
@@ -20,3 +20,6 @@ delimiter ;
 -- Testing
 -- select * from WorkflowInstances order by wfInstanceId desc limit 10;
 -- select * from ProcessInstances order by processInstanceId desc limit 10;
+
+-- Recovery
+-- delete from WorkflowInstances where wfInstanceId > 1000;
